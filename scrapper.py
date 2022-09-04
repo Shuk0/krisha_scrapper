7ab86876ff55d1aa41e9a39247437f80ae53a901
Vladimir <provodnikovvv@gmail.com>
2022-09-05 02:38:36 +0300

    Add CLI parser, prepare to feature 'read URL from file' so change check_cli_arg function and add terminate script

--- scrapper.py
+++ scrapper.py
@@ -1,35 +1,82 @@
+import argparse
+import re
 import requests
 from bs4 import BeautifulSoup
-from sys import argv
-import re
 
-def terminate_script(exit_code: int):
-    OUTPUT_MESSAGES = {1:"You missed an argument. It's expected a URL that should be grabbed.",
-        2:"Argument have to begin from 'https://krisha.kz'.",
-        3:"Too many arguments. It's expected just one argument which is URL to be grabbed.",
-        4:"Page is not available. Check connection and restart the script."}
+def cli_parser() -> argparse.ArgumentParser:
+    """ Function parse a command line. 
+        Function format: python scrapper.py [-h] [url] [-ff file_with_url], where:
+            - [-h] or [--help] optional argument. Description of ArgumentParser will be printed;
+            - [url] positional argument site utl for scrapping;
+            - [-ff file_with_url] or [--fromfile file_with_url] optional argument. PATH to file with site url.
+        Function return ArgumentParser object. 
+    """
+
+    cli_parser = argparse.ArgumentParser(description='Scrapper from krisha.kz')
+    cli_parser.add_argument('url', default=None, nargs='?',
+                            help='site url for scrapping. Have to start with symbols "https://krisha.kz"'
+                            )
+    cli_parser.add_argument('-ff', '--fromfile',  dest='file_with_url',
+                            help='''file with site url for scrapping.
+                            You have to specify the full path to file 
+                            or file have to be on the same folder'''
+                            )
+    args = cli_parser.parse_args()
+    
+    return [args.url, args.file_with_url]
+
+def check_cli_arg(command_line_arguments: list) -> list:
+    """ Function get command line arguments and check:
+            - that URL or PATH to file with URL was entered in a command line;
+            - that URL and PATH to file with URL didn't entered in a command line together.
+    """
+
+    if command_line_arguments[0] == None and command_line_arguments[1] == None:
+        terminate_script(1)
+    elif command_line_arguments[0] != None and command_line_arguments[1] != None:
+        terminate_script(2)
+    elif command_line_arguments[0] != None:
+        return check_url(command_line_arguments[0])
+    else:
+        url = extract_url_from_file(command_line_arguments[1])
+        return check_url(url)
+
+def check_url(url:str) -> str:
+    """Function check that URL starts with 'https://krisha.kz'"""
+
+    if not re.search(re.compile('^https://krisha.kz'), url):
+            print(url)
+            terminate_script(3)
+    else:
+        return url
+
+def extract_url_from_file(path:str) -> str:
+    """Function get PATH to file with site url than read and return first line"""
+    try:
+        with open(path, 'r') as file:
+            url = file.readline()
+            url = url.rstrip('\n')
+        return url
+    except IOError:
+        terminate_script(4)
+
+def terminate_script(exit_code: int) -> None:
+    """ Function get exit code, print terminate message from dictionary according exit code
+        and exit programm.
+    """
+
+    OUTPUT_MESSAGES = {
+        1 : '''You missed an argument. It's exspected an URL or path to file with URL that should be grabbed. 
+               Start the script with "-h" or "--help" optinal argument to get help''',
+        2 : '''Too many arguments. It's exspected just one argument which URL or path to file with URL to be grabbed.
+ 	            Start the script with "-h" or "--help" optinal argument to get help''',
+        3 : "Argument have to begin from 'https://krisha.kz'",
+        4 : "File not found. Check the path and restart the script.",
+        5 : "Page is not available. Check connection and restart the script."
+        }
     
     print(OUTPUT_MESSAGES[exit_code])
     exit(1)
-
-
-def check_cli_arg(command_line_arguments: list) -> str:
-    """ Function check:
-            - that URL was entered in a command line. If it wasn't than a user gets message and 
-                is prompted to provide URL as an argument;
-            - that URL starts with 'https://krisha.kz'. If it wasn't than a user gets message and 
-                is prompted to provide correct URL as an argument.
-    """
-    
-    if len(command_line_arguments) == 1:
-        terminate_script(1)
-    elif len(command_line_arguments) == 2:
-        cli_argument = command_line_arguments[1]
-        if not re.search(re.compile('^https://krisha.kz'), cli_argument.strip()):
-            terminate_script(2)
-    elif len(command_line_arguments) > 2:
-        terminate_script(3)
-    return cli_argument.strip()
 
 def grab_html_page(accepted_url: str, page_number: int) -> BeautifulSoup:
     """ Function scrap site page by url and return BeautifulSoup object."""
@@ -38,8 +85,8 @@ def grab_html_page(accepted_url: str, page_number: int) -> BeautifulSoup:
     
     try:
         page = requests.get(url, timeout=0.5)
-    except requests.ConnectionError as e:
-        terminate_script(4)
+    except requests.ConnectionError:
+        terminate_script(5)
     
     if page.status_code == 200:
         print(f'Page {page_number} scrapped. Status code: {page.status_code}', end = '\n')
@@ -56,11 +103,11 @@ def get_data(soup: BeautifulSoup, data: dict) -> int:
 
     URL = 'https://krisha.kz'
     # find all offers on page
-    text_data = soup.find('section',
+    list_with_data = soup.find('section',
                             class_='a-list a-search-list a-list-with-favs').findAll('div',
                                                          class_=re.compile('^a-card a-storage-live ddl_product'))
 
-    for each in text_data:
+    for each in list_with_data:
         # parse price of flat
         text_with_one_flat_price = ''.join(each.find('div',
                                          class_='a-card__price').text.strip()[:-1].strip().split())
@@ -96,7 +143,7 @@ def get_number_of_options(soup: BeautifulSoup) -> int:
     try:
         numbers_of_options = int(content_of_div_with_number.encode("ascii", "ignore")) # number of options
     except TypeError:
-        print("Can't parse number of options as extracted text is the following" + numbers_of_options)
+        print("Can't parse number of options as extracted text is the following" + content_of_div_with_number)
 
     return numbers_of_options
 
@@ -106,8 +153,6 @@ def get_cost_per_sq_m(data: dict) -> dict:
     cost_per_sq_m = {key: int(data[key][1]/data[key][2]) for key in data.keys()}
     
     return cost_per_sq_m
-
-
 
 def main():
     """ Function scrap site by URL which entered in command line.
@@ -119,12 +164,11 @@ def main():
 # https://krisha.kz/prodazha/kvartiry/almaty-aujezovskij-mkr-dostyk/?das[live.rooms]=1
 # https://krisha.kz/prodazha/kvartiry/almaty-aujezovskij-mkr-zhetysu-1/?das[live.rooms]=1 
 
-    command_line_arguments = argv #parse argument from CLI
+    command_line_arguments = cli_parser()
+    accepted_url = check_cli_arg(command_line_arguments)
     page_number = int(1)
     numbers_of_added_options = int(0)
     data = {}
-     
-    accepted_url = check_cli_arg(command_line_arguments)
 
     soup = grab_html_page(accepted_url, page_number)
     numbers_of_options = get_number_of_options(soup)
@@ -156,4 +200,3 @@ def main():
 if __name__ == '__main__':
     
     main()
-    
